c4a01d7c5f5465cff3a778332f768ad18f3563b9
quick919 <qquick919@gmail.com>
2018-08-17 06:37:42 +0900

    Uncommitted.

---
+++ test/src/main/resources/assets/js/modules/selectable-publisher.js
@@ -0,0 +1,23 @@
+export default {
+  data() {
+    return {
+      options: [{
+          id: 1,
+          text: "Amazon"
+        },
+        {
+          id: 2,
+          text: "O'Reilly"
+        },
+        {
+          id: 3,
+          text: "gihyo"
+        },
+        {
+          id: 4,
+          text: "Leanpub"
+        }
+      ]
+    };
+  }
+}
